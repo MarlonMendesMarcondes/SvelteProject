efc9bd1b23a44b36a5b71e6cf1fcfaaed5c424da
Marlon Mendes Marcondes <marlon.836@hotmail.com>
2023-11-30 19:18:06 -0300

    Criando Filtros em Listagens

--- src/routes/projetos/+page.ts
+++ src/routes/projetos/+page.ts
@@ -1,9 +1,15 @@
 export function load(){
-    console.log("OLA mundo");
     return{
+        categories:[
+            'Todos',
+            'Estrutura Metalica',
+            'Laudos Tecnicos',
+            'Consultoria'
+        ],
+        
         items:[ 
-            {title:'Item 1', category:'Componentes', image:''},
-            {title:'Item 1', category:'Componentes', image:''},
+            {title:'Cobertura de Centro de distribuiçao', category:'Estrutura Metalica', image:'https://images.pexels.com/photos/12951630/pexels-photo-12951630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
+            {title:'Arvore de Natal da Cidade de Maria Helena', category:'Estrutura Metalica', image:'https://images.tcdn.com.br/img/img_prod/573228/arvore_de_natal_luminosa_gigante_led_fabricada_em_estrutura_metalica_constelation_202841_1_bda50503a49a8cc4c5a52e799db46ae5.jpg'},
             {title:'Item 1', category:'Componentes', image:''},
             {title:'Item 1', category:'Componentes', image:''}
         ]
